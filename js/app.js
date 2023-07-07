f0c358562cd0f624347257c92bfba40c26402dc7
Ozckar <96064097+Ozckar07@users.noreply.github.com>
2023-07-06 22:53:19 -0500

    saludo interaccion con hover

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ check.addEventListener('click', idioma);
 function idioma() {
     let id = check.checked;
     if (id == true) {
-        location.href = "es/index.html";
+        location.href = "../es/index.html";
     } else {
         location.href = "../index.html";
     }
